b265a13c0177d9c28c86e97ecc31d5acae01e470
JJKETO <jjketo@utu.fi>
2017-07-20 17:04:03 +0300

    Update neat.R

--- R/neat.R
+++ R/neat.R
@@ -964,7 +964,7 @@ NEATSimulation.RunSingleGeneration <- function(simulation, createVideo=F, videoP
   print("Starting simulations...")
   counter <- 1
   nTot <- calcTotalNumOfGenomes(simulation)
-  foreach::foreach(i = icount(length(simulation$Pool$species)),.packages = c("Game2048")) foreach::%dopar%{
+  foreach::foreach(i = icount(length(simulation$Pool$species)),.packages = c("Game2048")) %dopar%{
     for(j in seq(1,length(simulation$Pool$species[[i]]$genomes))){
       simulation <- simulationRunner(simulation,i,j,F,100*counter/nTot,generationSeed = generationSeed)
       counter <- counter + 1
